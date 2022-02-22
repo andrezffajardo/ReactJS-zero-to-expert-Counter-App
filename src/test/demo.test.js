e8676abb4a4160c8796e589a9e3047b010eb2f42
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-02-22 17:37:11 -0500

    Jest - Expect - toBe

--- src/test/demo.test.js
+++ src/test/demo.test.js
@@ -1,9 +1,17 @@
 
-test( 'should be true', () => {
 
-     const isActive = true;
+describe('Pruebas en el archivo demo.test.js', () => {
 
-     if ( isActive ) {
-         throw new Error( 'Its not active' )
-     }
-})
+    test( 'deben ser iguales los string', () => {
+
+        //1. Inicializacion
+        const mensaje = 'Hi World';
+
+        //2. Estimulo
+        const mensaje2 = `Hi World`;
+
+        //3. Observar comportamiento
+        expect( mensaje ).toBe( mensaje2 );
+    })
+
+});
