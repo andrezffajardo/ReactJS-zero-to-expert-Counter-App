11226e94f13ef66b9a6f81e15549b5799f31c81d
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-02-22 10:23:00 -0500

    Exercise component CounterApp

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FirstApp from "./FirstApp";
 import './index.css';
+import CounterApp from "./CounterApp";
 
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <FirstApp greet="Hi World"/>, divRoot );
+ReactDOM.render( <CounterApp value={ 321 }/>, divRoot );
