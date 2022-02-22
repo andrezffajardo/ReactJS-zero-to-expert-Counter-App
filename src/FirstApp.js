3415c4e33c7f72f16abde17e467acd6a87c72829
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-02-21 19:07:21 -0500

    Return elements component

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const FirstApp = () => {
-    return <h1>Hi World</h1>;
+    return (
+        <>
+            <h1>Hi World</h1>
+            <p>My first App</p>
+        </>
+
+    );
 }
 
 export default FirstApp;
