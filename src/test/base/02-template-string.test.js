70f088d7c5bea896f424cc46b48e07b01e2c7539
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-02-22 18:27:11 -0500

    Test on 02-template-string

---
+++ src/test/base/02-template-string.test.js
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom';
+import { getSaludo } from '../../base/02-template-string';
+
+
+
+describe('Pruebas en 02-template-string.js', () => {
+
+    test('getSaludo debe retornar Hi Andrew!', () => {
+
+        const nombre = 'Andrew';
+
+        const saludo = getSaludo( nombre );
+
+        expect( saludo ).toBe( 'Hi ' + nombre + '!');
+    });
+
+    // getSaludo debe etornar Hi Pipe! si no hay argumento en nombre
+    test('getSaludo debe retornar Hi Pipe!', () => {
+
+        const saludo = getSaludo();
+
+        expect( saludo ).toBe( 'Hi Pipe!');
+    });
+
+
+
+})
